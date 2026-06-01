0258b27c41825e23b6cd2ba88fc51c8c098bbb16
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-06-01 09:14:53 +0600

    generics.ts is updated

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -1,25 +1,42 @@
 // Without generics
-
 function logString(data: string): void {
     console.log(data);
 }
-logString('Hello world')
+logString('Hello world');
 
 function logNumber(data: number): void {
     console.log(data);
 }
-logNumber(252)
+logNumber(252);
 
 function logBoolean(data: boolean): void {
     console.log(data);
 }
-logBoolean(true)
+logBoolean(true);
 
 // With generics (super reusable)
-
 function logData<T>(data: T): void {
     console.log(data);
 }
-logData('Hello world')
-logData(252)
-logData(false)
+logData('Hello world');
+logData(252);
+logData(false);
+logData(['A', 'B', 'C']);
+logData([12, 25, 89]);
+
+// Generics interface
+interface IApi<T> {
+    success: boolean;
+    data: T;
+}
+const stringResponse: IApi<{ name: string }> = {
+    success: true,
+    data: {
+        name: 'Rana',
+    },
+};
+
+const numberResponse: IApi<number> = {
+    success: true,
+    data: 121,
+};
